60c845dff55b4eeaca8074566c66bce5e4b20a22
Kana Laptop <willian.kana@tecsup.edu.pe>
2023-11-22 17:27:05 -0500

    primer commit

---
+++ resources/views/fotos/fotos.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="{{ route('subirFoto') }}" method="POST" enctype="multipart/form-data" class="row g-3">
+        @csrf
+        <h5>Subir una foto</h5>
+            <label>Descripcion</label>
+            <input class="form-control" type="text" name="descripcion" placeholder="Agregue una descripcion">
+            <input class="form-control" type="file" name="foto">
+            <button type="submit" class="btn btn-primary">Subir</button>
+    </form>
+    <div class="row row-cols-3 mt-5">
+        @foreach ($fotos as $foto)
+        <div class="card">
+            <img height="200" src="/foto/{{$foto->ruta}}" alt="">
+            <div class="card-body">
+                <p class="card-text">{{$foto->descripcion}}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <small class="text-muted">{{$foto->created_at}}</small>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
